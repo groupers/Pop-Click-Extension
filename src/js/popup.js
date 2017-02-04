e788e5e6599c972c6762086956fef87ee3177b97
groupers <phileas.hocquard@gmail.com>
2017-02-04 17:40:17 +0000

    Ability to send a normal post

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,8 +1,11 @@
 
 var chips = document.getElementsByClassName('chip')
+var genders = ["Female","Male","Other","Irrelevant"]
+var sizeSwitch = false;
 $(document).ready(function() {
 	$('select').material_select();
 	$('.chips').material_chip();
+	$('#sparkles').hide();
 	$('.chips-initial').material_chip({
 		data: [{
 			tag: 'New & Media',
@@ -43,31 +46,35 @@ $(document).ready(function() {
 	});
 	$('.chip > i').hide();
 	$('.chips > input').remove();
-	$('.chips').on('chip.add', function(e, chip){
-    // you have the added chip here
-});
-
-	$('.chips').on('chip.delete', function(e, chip){
-    // you have the deleted chip here
-});
-
-	$('.chips').on('chip.select', function(e, chip){
-
+	var chips_count = 0;
+	$('.chips').on('chip.select', function(e, chip) {
 		Array.from(chips).forEach(function(element) {
 			if(chip.tag == element.outerText){
 				element.className = "chip";
-				element.value = (element.value+1)%2;
-				if (element.value) {
-					element.style.backgroundColor = '#26a69a';
-					element.style.color = 'white';
+				if ((element.value+1)%2) {
+					if(chips_count < 3){
+						element.value = (element.value+1)%2;
+						chips_count++;
+						element.style.backgroundColor = '#26a69a';
+						element.style.color = 'white';
+					}
 				} else {
+					element.value = (element.value+1)%2;
+					chips_count--;
 					element.style.backgroundColor = '#e4e4e4';
 					element.style.color = 'rgba(0,0,0,0.6)';
 
 				}
-				console.log(element.value)
-			}
+				console.log(chips_count)
+				if(chips_count > 2) {
+					$('#sparkles').show();
+				} else {
+					$('#sparkles').hide();
+				}
+			}		
 		});
+		var c = (3 - chips_count);
+		document.getElementById('number_of_select').innerText = c;
 	});
 });
 
@@ -75,32 +82,56 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('initialForm').addEventListener('submit', initialPost)
    // When the user ticks to agree on the contract
    var signing = document.getElementById('signing')
+
+   var ran_age = document.getElementById('ran_age')
+   var in_age = document.getElementById('in_age')
+
    signing.addEventListener('click', function() {
    	if (signing.value == undefined){
    		signing.value = 0;
    	}
    	signing.value = (+(signing.value) + 1)%2
-   	console.log(signing.value)
+   	if(signing.value){
+   		setTimeout(flipInitialState, 500);
+   	}
    }); 
-   var ran_age = document.getElementById('ran_age')
-   var in_age = document.getElementById('in_age')
+   ran_age.value = 18;
+   in_age.value = 18;
    ran_age.addEventListener('change', function () {
    	in_age.value = ran_age.value
    })
    in_age.addEventListener('change', function () {
    	ran_age.value = in_age.value
    })
-});
+ });
 
-var chipHandler = function(element) {
-	element.preventDefault;
-    // var attribute = this.getAttribute("data-myattribute");
-    // alert(attribute);
-    console.log("*boƒom*")
-};
+function flipInitialState(){
+	var middle = document.getElementById('middle');
+	var bottom = document.getElementById('bottom');
+
+	if(!sizeSwitch){
+		bottom.style.height = '70%';
+		middle.style.height = '20%';
+		$('#initial-1').hide()
+		$('#initial-2').hide()
+		$('#interests').show()
+	} else {
+		bottom.style.height = '60%';
+		middle.style.height = '30%';
+		$('#initial-1').show()
+		$('#initial-2').show()
+		$('#interests').hide()
+	}
+	sizeSwitch = !sizeSwitch;
+}
+
 function initialInterests(){
 	var interests = []
-
+	Array.from(chips).forEach(function(element) {
+		if(element.value == 1 ){
+			interests.push(element.innerText)
+		}
+	})
 	return interests
 }
 
@@ -126,17 +157,27 @@ function initialPost(){
 		HH='0'+HH;
 	}
 	today = yyyy+'-'+mm+'-'+dd+' '+HH+':'+MM
-	var age = 25;
-	var interests = ['food','movies']
+	var age = document.getElementById('in_age').value
+	var gender = genders[document.getElementById('gender').value-1]
+	var signed = document.getElementById('signing').value
 	console.log(today);
+	postAction(today, age, initialInterests(), gender, signed, initialResponse);
+}
 
-	postAction(today, age, interests, signed, initialResponse);
+function displayElement(obj) {
+	var obj = document.getElementById(obj);
+	if (obj.style.visibility == 'visible') {
+		obj.style.visibility = 'hidden';
+	}
+	else {
+		obj.style.visibility = 'visible';
+	}
 }
 
 function initialResponse(str){
 	console.log(str)
 }
-function postAction(logtime, age, interests, signed, callback){
+function postAction(logtime, age, interests, gender, signed, callback){
 	var postUrl = 'http://localhost:8000/popclick/api/create/';
     // Set up an asynchronous AJAX POST request
     var xhr = new XMLHttpRequest();
@@ -152,14 +193,14 @@ function postAction(logtime, age, interests, signed, callback){
                 // statusDisplay.innerHTML = 'Saved!';
                 callback(xhr.responseText);
                 // window.setTimeout(window.close, 1000);
-            } else {
+              } else {
                 // Show what went wrong
                 // statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
+              }
             }
-        }
-    };
+          };
     // };JSON.stringify(initialInterests)
-    xhr.send(JSON.stringify({"logtime":logtime,"age":age,"interests":interests}));
+    xhr.send(JSON.stringify({"logtime":logtime,"age":age,"gender":gender,"interests":interests}));
     // statusDisplay.innerHTML = 'Saving...';
 
-}
+  }
