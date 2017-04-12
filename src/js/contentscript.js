c68cde8299fa4b88e1d8be3adb9334f759d68e00
groupers <phileas.hocquard@gmail.com>
2017-04-12 22:23:25 +0100

    Content script doc

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -23,22 +23,24 @@ var sentObjects = new Map();
 * @params {sender}
 * @params {sendResponse}
 * @message action handling:
-*	- refresh_dialog
-*	- show_dialog
-*	- sendpage_info
-*	- feedback_info
+*	- refresh_dialog from -background.js
+*	- show_dialog	from -popup&popup_control.js
+*	- sendpage_info	from -background.js
+*	- feedback_info from -background.js
 **/
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-
+	// Message received suggests that the dialogBox should refresh (From background)
 	if(msg.action == 'refresh_dialog') {
 		generateDialogContent(msg.url);
+		searchUtilityItems();
 		window.localStorage['location'] = JSON.stringify([document.location.hostname, document.location.pathname, document.location.href])
 	}
-
+	// Message received demands that the dialogBox becomes visible (From popup)
 	if(msg.action === 'show_dialog') {
 		document.getElementById('TheDialogBox').style.display = ''
 	}
 
+	// Message received demands that the list of selectable items should be sent as a Response
 	if(msg.action == 'sendpage_info') {
 		var objects = [];
 		for (i = 0 ; i < document.getElementsByTagName('a').length && i < 500; i++) {
@@ -55,34 +57,43 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
 		sentObjects[document.location.href] = objects;
 		sendResponse({objects: objects});
 	}
+	// Message received demands that a certain set of elements should be displayed as toasts
 	if(msg.action == 'feedback_info') {
 		var toastall = false;
-		// Add option if toasts should be visible
+		// If toasts should appear for the given time (avoiding constant refresh backlashes)
 		if(feedback_info_timestamp === null || (new Date() - feedback_info_timestamp)/ 1000.0 > 2.0){
 			feedback_info_timestamp = new Date();
 			feedback_info_link = document.location.href;
 			toastall = true;
 		}
+		// If we should set new toasts
 		if(toastall == true) {
-			var timeout = 7000, changed = false;
+			var timeout = 7000
+			// If we have received more than 0 toasts
 			if (msg.numbers != -1 && sentObjects[document.location.href].length >0) {
+				// If we receive an update request than we must destroy all elements
 				if(msg.update && document.getElementsByClassName('iziToast-body').length > 0){
 					iziToast.destroy();
 				}
-				if(document.getElementsByClassName('iziToast-body').length == 0 || changed == true) {
+				// If there is no existing toasts
+				if(document.getElementsByClassName('iziToast-body').length == 0) {
+					// Create up to 5 toasts
 					for(i=0; i<msg.numbers.length && i<5; i++) {
 						var elem = sentObjects[document.location.href][msg.numbers[i]]
-						// Have to fix the fact of being sent back a random list with an item 0
+						// Verify that the matching item contains text or is existent
 						if(typeof elem != 'undefined' && elem[2].replace(/\s/g,' ').length != 0){
 							var message = elem[2].trim();
+							// If the text is too long
 							if (message.length > 15){
 								message = message.substring(0,15)+"...";
 							}
+							// For the top 2 elements set a longer duration
 							if(i<2){
 								timeout = 100000;
 							}else{
 								timeout = 10000;
 							}
+							// Display the toasts
 							iziToast.show({
 								title: '['+i+']',
 								message: message,
@@ -92,9 +103,6 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
 									console.log(instance)
 								},
 								onClose: function(instance, toast, closedBy){
-						        console.info('closedBy: ' + closedBy); // tells if it was closed by 'drag' or 'button'
-
-						        console.log("item removed"+instance['title'])
 						        delete iziToasts[instance['title']]
 						    }
 						});
@@ -122,56 +130,74 @@ var p = 0, a = 1, d = 2, ac = 6, ul = 4, li = 5, script = 7, link = 8;
 * @return {string} html element
 **/
 function e(elementShort, text, href, ID, classname, order, kind){
+	// The returned DOM formatted element
 	var returnvalue = "";
+	// If the their is no text field or an empty string then return.
 	if (typeof text === 'undefined' || text.length == 0) return ;
+	// Trim the text
 	text = text.trim();
+	// If any of the following attribute does not exist set them to an empty string
 	if (typeof ID === 'undefined') { ID = ''; }
 	if (typeof href === 'undefined') { href = ''; }
 	if (typeof classname === 'undefined') { classname = ''; }
 	if (typeof order === 'undefined') { order = ''; }
 	if (typeof kind === 'undefined') { kind = ''; }
 	if (typeof text === 'undefined') { text = ''; }
+	// For the specific element  choose corresponding HTML string embedding
 	switch(elementShort) {
 		case 0:
 		returnvalue = "<p>" + text + "</p>";
 		break;
+		// anchor
 	    case 1:// set 2 options
+	    // If the string is too long reduce its size
 	    if (text.length > 25) {
 	    	text = text.substring(0,25)+"..." ;
 	    }
+	    // If the element has an order then it is a recommendation
 	    if (order != undefined) {
 	    	text +="<span> ["+order%10+"]</span>";
 	    	ID = "DialogBoxAnchor"+order%10;
 	    }
 	    returnvalue = (ID === "")? '<a href="' + href +'">' + text + "</a>" : '<a id="' + ID + '" href="' + href +'">' + text + '</a>'
 	    break;
+	    // div
 	    case 2:
 	    returnvalue = (ID === "")? '<div>' + text + "</div>" : '<div id="' + ID + '">' + text + '</div>'
 	    break;
+	    // ul
 	    case 4:
 	    returnvalue ='<ul>' + text + '</ul>'
 	    break;
+	    // li
 	    case 5:
 	    returnvalue = '<li>' + text + '</li>'
 	    break;
+	    // anchor with onclick
 	    case 6:
 	    returnvalue = '<a id="' + ID + '" onclick="' + href +'">' + text + '</a>'
 	    break;
+	    // script
 	    case 7:
 	    returnvalue = '<script src="'+ href +'"></script>'
 	    break;
+	    //  stylesheet
 	    case 8:
 	    returnvalue = '<link rel="stylesheet" href="' +href + '">'
 	    break;
+	    // nothing
 	    case 10:
 	    return
+	    // Return the text otherwise
 	    default:
 	    returnvalue = text;
 	    break;
 	}
+	// If the element has a class then add its attribute
 	if(classname) {
 		returnvalue = returnvalue.splice(returnvalue.indexOf('>'), 0, ' class="' + classname + '"');
 	}
+	// If the element has a kind then add its attribute
 	if(kind) {
 		returnvalue = returnvalue.splice(returnvalue.indexOf('>'), 0, ' kind="' + kind + '"');
 	}
@@ -185,9 +211,11 @@ function e(elementShort, text, href, ID, classname, order, kind){
 *
 **/
 function generateDialogContent(url) {
+	// If there is no url set the url to an empty string
 	if (typeof url === 'undefined') { 
 		url = ''; 
 	} else {
+		// create a JSON object containing the string
 		var array = new Array();
 		array[0] = url;
 		stringifiedArray = JSON.stringify(array);
@@ -204,15 +232,19 @@ function generateDialogContent(url) {
 		var dialogbuttons = document.getElementById("ButtonCollection");
 		// If the dialogBox is empty generate content
 		if(dialogbuttons != null && dialogbuttons.firstChild != null){
+			// While there is a child remove all elements
 			while (dialogbuttons.firstChild) {
 				dialogbuttons.removeChild(dialogbuttons.firstChild);
 			}
 			var randomListOfAnchors = []
 			// Select the 10 first of elements of the list (random procedure)
 			for (i = 0 ; i < document.getElementsByTagName('a').length; i++) {
+				// If the element in the list doest not contain an href that is the same as the suggested items
+				// Add it to the collection of random anchors
 				if(!highest_clicks_href.contains(document.getElementsByTagName('a')[i])) {
 					randomListOfAnchors.push(i);
 				}
+				// Assuring we have text for the given suggestion
 				if(highest_clicks_text.length > (i+1)){
 					if(highest_clicks_text[i] == 'not-found' || highest_clicks_href[i] == ""){
 						highest_clicks_text.remove(i)
@@ -225,7 +257,6 @@ function generateDialogContent(url) {
 			// Creates an an anchor tag corresponding to the conventioned style with the given parameters
 			for (i = 0; i < 10; i++){
 				var text, href;
-				console.log(highest_clicks_text[i])
 				if(highest_clicks_text[i] != undefined) {
 					text = highest_clicks_text[i];
 					href = highest_clicks_href[i];
@@ -257,12 +288,11 @@ function generateDialogContent(url) {
 	});
 }
 
-main()
 /**
-* @constructor 
-* 
+*Initialises the listofnameElements which is used by the search utility
 **/
-function main(){
+function searchUtilityItems(){
+	listofnameElements = [];
 	for (i = 0; i < 500 && i < document.getElementsByTagName('a').length; i++) {
 		var currentAnchor = document.getElementsByTagName('a')[i];
 		var currentElement = (currentAnchor.text).replace(/\s/g,' ');
@@ -271,12 +301,30 @@ function main(){
 			mapOfElements.set(currentElement,currentAnchor.href);
 		}
 	}
+}
+
+main()
+/**
+* Main method
+* @constructor 
+* 
+**/
+function main(){
+	searchUtilityItems();
 	createDialogBox()
 	generateDialogContent();
 }
 
+/**
+*Creates the DialogBox
+* @actions:
+*	-Initialises Awesomplete inputField
+*	- Adds a MutationObserver method
+**/
 function createDialogBox(){
+	// DOM formatted input field
 	var inputfield = '<input id="AwesompleteInputfield" class="awesomplete" data-autofirst placeholder="Insert Text to find what you wish for :"/>';
+	// Creates the Dialog Box
 	var dim_div = document.createElement("div");
 	document.body.appendChild(dim_div);
 	dim_div.id = "pagecover"
@@ -289,7 +337,7 @@ function createDialogBox(){
 	div.style.left = "50px";
 	div.style.display = "none";
 	var input = document.getElementById("AwesompleteInputfield");
-	
+	// Initialises the awesomplete search and filter
 	new Awesomplete(input, {
 		// Remove duplicates
 		list: listofnameElements.filter( function( item, index, inputArray ) {
@@ -305,7 +353,7 @@ function createDialogBox(){
 			return ((text.toLowerCase()).includes(input.toLowerCase()));
 		}
 	});
-
+	// Allows monitor the DialogBox activity
 	var dialogBoxVisible = false;
 	// Observer for when the div element has it's class attribute altered
 	/** Observing the visibility of the dialog box **/
@@ -320,9 +368,7 @@ function createDialogBox(){
 	observerDisplay.observe(div, { attributes: true });
 }
 
-/** vvvvvvvvvvv vvvvvvvvvvv vvvvvvvvvvv vvvvvvvvvvv vvvvvvvvvvv vvvvvvvvvvv vvvvvvvvvvv vvvvvvvvvvv **/
 // Injecting script
-
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('src/js/script.js');
 s.onload = function() {
